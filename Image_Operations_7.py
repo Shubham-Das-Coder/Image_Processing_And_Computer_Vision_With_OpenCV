55f34456a77d3d2bc10c857fed52f9d0ce07ecf2
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-30 19:26:04 +0530

    Delete Image_Operations_7.py

--- Image_Operations_7.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img1=cv2.imread("E:\College Memories\WhatsApp Image 2021-12-28 at 4.55.56 PM.jpeg",0)
-img1=cv2.resize(img1,(900,600))
-print(img1)
-cv2.imshow("original",img1)
-cv2.waitKey(3000) # The screen holds the image for 3 seconds or 3000 milliseconds
-cv2.destroyAllWindows()
