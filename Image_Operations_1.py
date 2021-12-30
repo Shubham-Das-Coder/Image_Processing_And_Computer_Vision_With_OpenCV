263235ee52b88fa09102fe7a79406362aca18982
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-30 19:26:46 +0530

    Add files via upload

---
+++ Image_Operations_1.py
@@ -0,0 +1,7 @@
+import cv2 # openCV use as cv2 in python
+
+img1=cv2.imread("E:\College Memories\WhatsApp Image 2021-12-28 at 4.55.56 PM.jpeg")
+print(img1)
+cv2.imshow("Original Image",img1)
+cv2.waitKey()
+cv2.destroyAllWindows()
