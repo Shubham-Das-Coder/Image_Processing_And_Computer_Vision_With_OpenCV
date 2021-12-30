7cc7ce5dbfea6e6805a6c026dd24a2c442e92d91
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-30 21:30:21 +0530

    Add files via upload

---
+++ Video_Operations_1.py
@@ -0,0 +1,6 @@
+import cv2
+
+cap = cv2.VideoCapture("E:\College Memories\WhatsApp_Video_2021-12-28_at_4.05.46_PM.mp4") # Accessing the video
+print("cap",cap)
+
+# cap is an object where we have read a video and stored in it
