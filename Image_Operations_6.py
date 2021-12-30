e17911226a3d53648d67ca9cce1966e5eac3dcab
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-30 19:14:20 +0530

    Delete Image_Operations_6.py

--- Image_Operations_6.py
+++
@@ -1,8 +0,0 @@
-import cv2 #openCV use as cv2 in python
-
-img1=cv2.imread("E:\College Memories\WhatsApp Image 2021-12-28 at 4.55.56 PM.jpeg",0) #1 indicates that the image is greyscale mode
-img1=cv2.resize(img1,(900,600)) #Resizing the image
-print(img1)
-cv2.imshow("original",img1)
-cv2.waitKey(0) #Even if we dont pass any number in it, it takes 0 by default
-cv2.destroyAllWindows()
