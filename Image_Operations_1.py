e41e3a2888779f893f7b00ab979b8ad839f77748
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-30 19:25:11 +0530

    Delete Image_Operations_1.py

--- Image_Operations_1.py
+++
@@ -1,7 +0,0 @@
-import cv2 # openCV use as cv2 in python
-
-img1=cv2.imread("E:\College Memories\WhatsApp Image 2021-12-28 at 4.55.56 PM.jpeg")
-print(img1)
-cv2.imshow("original",img1)
-cv2.waitKey()
-cv2.destroyAllWindows()
