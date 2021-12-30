d4d5db0c13f5c91a00fbdb50e83fe075a622e563
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-30 20:04:21 +0530

    Delete Image_Operations_10.py

--- Image_Operations_10.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img1=cv2.imread("E:\College Memories\WhatsApp Image 2021-12-28 at 4.55.56 PM.jpeg",-1) 
-img1=cv2.resize(img1,(900,600))
-img1=cv2.flip(img1,0) # It flips the image by 180 degree
-print(img1)
-cv2.imshow("Unchanged Image",img1)
-cv2.waitKey(3000)
-cv2.destroyAllWindows()
