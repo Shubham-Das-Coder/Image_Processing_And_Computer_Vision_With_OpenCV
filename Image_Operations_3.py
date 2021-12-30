d23eb46a3be68246a48b05d6def3b6a9a1f55742
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-30 19:25:31 +0530

    Delete Image_Operations_3.py

--- Image_Operations_3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img1=cv2.imread("E:\College Memories\WhatsApp Image 2021-12-28 at 4.55.56 PM.jpeg",1) # 1 indicates that the image is coloured, and by default its value is 1
-img1=cv2.resize(img1,(900,600))
-print(img1)
-cv2.imshow("original",img1)
-cv2.waitKey()
-cv2.destroyAllWindows()
