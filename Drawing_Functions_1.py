d6a23e5b5fff97c9cc34ae08e13eb7c041c69fef
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-07-17 17:00:18 +0530

    Delete Drawing_Functions_1.py

--- Drawing_Functions_1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-img=cv2.imread("MSD_WC_Six.jpg")
-#Here line accept 5 parameter (img,starting,ending,color,thickness)
-img=cv2.line(img,(0,0),(200,200),(154,92,424),8) #Color format is in BGR
-cv2.imshow("Res",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
