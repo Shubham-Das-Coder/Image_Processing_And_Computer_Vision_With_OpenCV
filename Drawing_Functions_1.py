dae09673c7663b67614a12ab373f84910561ebf2
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-07-17 17:00:38 +0530

    Add files via upload

---
+++ Drawing_Functions_1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+img=cv2.imread("MSD_WC_Six.jpg")
+#Line accepts 5 parameter (img,starting,ending,color,thickness)
+img=cv2.line(img,(0,0),(200,200),(154,92,424),8) #Color format is in BGR
+cv2.imshow("Res",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
