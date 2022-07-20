b6b801058cb912f910a062d8801af0c15fdae8e8
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-07-20 22:06:41 +0530

    Add files via upload

---
+++ Drawing_Functions_5.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+img=cv2.imread("MSD_WC_Six.jpg")
+#Rectangle accepts 5 parameters (img,starting,ending,color,thickness)
+
+img=cv2.rectangle(img,(384,10),(510,128),(128,0,225),-1) #If we make thickness -1, then the rectangle gets filled with the colour
+cv2.imshow("Res",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
