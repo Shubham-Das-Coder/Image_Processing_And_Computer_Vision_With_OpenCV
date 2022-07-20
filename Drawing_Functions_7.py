85d06759574cb6676dd21e90044cd74b571688bb
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-07-20 22:07:18 +0530

    Delete Drawing_Functions_7.py

--- Drawing_Functions_7.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import cv2
-img=cv2.imread("MSD_WC_Six.jpg")
-img=cv2.circle(img,(447,125),63,(214,225,0),-5) #If we make thickness -1, then the circle gets filled with the colour of radius 1 unit
-cv2.imshow("Res",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
